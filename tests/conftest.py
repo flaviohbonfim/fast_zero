e98fc067d35109de41e40d694a3eb170a1876d46
Flávio Henrique Bonfim <flaviohbonfim@live.com>
2025-03-29 14:53:44 -0300

    format conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from contextlib import contextmanager
-from datetime import datetime
+
 from fast_zero.app import app
 from fast_zero.models import table_registry
 
@@ -31,7 +33,6 @@ def mock_db_time():
 
 @contextmanager
 def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
-
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
